35a1e785d8677a12f37093c55bdd0b05170fcd97
milabajos10 <milabajos49@gmail.com>
2023-06-22 12:14:46 +0200

    Config typescript finally

--- src/ts/index.js
+++ src/ts/index.js
@@ -1,3 +1,10 @@
 "use strict";
 //
 console.log("hello from type");
+document.addEventListener('DOMContentLoaded', () => {
+    const carritoSpan = document.getElementById('carrito');
+    const numeroCarrito = 5;
+    if (carritoSpan) {
+        carritoSpan.innerText = numeroCarrito.toString();
+    }
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,14 @@
 //
 console.log("hello from type");
+document.addEventListener('DOMContentLoaded', () => {
+    const carritoSpan = document.getElementById('carrito');
+    const numeroCarrito = 5;
+  
+    if (carritoSpan) {
+      carritoSpan.innerText = numeroCarrito.toString();
+    }
+  });
+  
+
+
+
